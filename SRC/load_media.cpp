a87de09f7765ec4bf24014c7ff511332c5cee405
o00o00o <stevenhelpme@gmail.com>
2016-08-09 22:01:29 -0700

    started text overlay

--- SRC/load_media.cpp
+++ SRC/load_media.cpp
@@ -18,7 +18,7 @@ bool load_media() {
   else {
     //render texture
     SDL_Color text_color = { 255, 255, 255, 255 };
-    if ( !g_text_texture.load_from_rendered_text( "hello from the other side ", text_color ) ) {
+    if ( !g_text_texture.load_from_rendered_text( "hello from the other side\n\nIs this any good? ", text_color ) ) {
       printf( "Failed to render text texture\n" );
       success = false;
     }
